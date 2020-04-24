fc2375fdfd10c028ad14eb2a982125928ef5ad62
Zepic <horto@hotmail.no>
2020-04-24 12:59:44 +0200

    fighting funker bra

--- Fighting/lib.js
+++ Fighting/lib.js
@@ -1,51 +1,85 @@
 //model
-let log = console.log
-let pHitpoints = 500;
-let bHitpoints = 9999;
-const attackButton = document.getElementById("attackId")
-
-let playerAttack = 420
-const bossAttack = 69
+let log = console.log;
+const attackButton = document.getElementById("attackId");
+const powerUpButton = document.getElementById("powerUpId");
+let playerAttack = 400;
+const bossAttack = 100;
+let healCooldown = 3;
 //view
 function show() {
-    ShowDiv.innerHTML = KAKE
+  const showDiv = document.getElementById("showDiv");
 }
 //controller
 function attack() {
-let x = Math.ceil(Math.random() * 3);
-let z = Math.ceil(Math.random() * 10);
-if (z == 3) bHitpoints -= playerAttack * 3;
-else bHitpoints -= playerAttack;
+  let pHitpoints = document.getElementById("playerHealth");
+  let bHitpoints = document.getElementById("bossHealth");
+  let x = Math.ceil(Math.random() * 5);
+  let z = Math.ceil(Math.random() * 10);
+  healCooldown++;
+  log(healCooldown)
+  if (z == 3) {
+    bHitpoints.value -= playerAttack * 3;
+    showDiv.innerHTML +=`You crit the boss for ${playerAttack * 3} damage! <br>`
+  }
+  else {
+    bHitpoints.value -= playerAttack;
+    showDiv.innerHTML +=`You hit the boss for ${playerAttack} damage! <br>`
+  }
 
-if (z >= 9)  playerAttack *= 1,2;
+  if (x != 5) {
+    pHitpoints.value -= bossAttack;
+    showDiv.innerHTML += `The boss sliced you for ${bossAttack}<br>`
+  } 
+  else {
+    showDiv.innerHTML += `You narrowly dodged the nurses vicious swipe <br>`
+  }
 
-if (x != 3) pHitpoints -= bossAttack;
-
-if (pHitpoints <= 0 || bHitpoints <= 0){
-
+  if (pHitpoints.value <= 0 || bHitpoints.value <= 0) {
     document.getElementById("attackId").style.display = "none";
+    document.getElementById("healId").style.display = "none";
+    document.getElementById("powerUpId").style.display = "none";
+    if (pHitpoints.value <= 0) showDiv.innerHTML += `You died`;
+    else if (bHitpoints.value <= 0) showDiv.innerHTML += `The boss is defeated`
+  }
+
 }
-/*
-TODO
-heal + heal cooldown 3 runder 
-69 + 69 = 138
-69 + 69 + 69 = 207
 
-gjøre 1,2 multiplier til charge up move
-
-dynamic health bars
-current dmg 
-heal amount
-
-
-*/
-log (x, "Player Dodge")
-log (z, "Attack Increase")
-log (playerAttack, "Attack")
-log (pHitpoints, "Player Hitpoints")
-log (bHitpoints, "Boss Hitpoints")
+function heal() {
+  const healButton = document.getElementById("healId");
+  let pHitpoints = document.getElementById("playerHealth");
+  let x = Math.ceil(Math.random() * 5);
+  if (pHitpoints.value >= 500 || pHitpoints.value <= 0) return;
+  log(healCooldown, "skal være 3")
+  if (healCooldown >= 3) {
+    healCooldown = 0;
+    pHitpoints.value += 100;
+    showDiv.innerHTML += `You healed for 100HP <br>`
+    log(healCooldown,"skal være 0")
+    if (x != 5) {
+      pHitpoints.value -= bossAttack;
+      showDiv.innerHTML += `The boss hit you for ${bossAttack} damage <br>`
+    }
+    else {
+      showDiv.innerHTML += `You narrowly dodged the nurses vicious swipe <br>`
+    }
+  }
 }
-function rReload() {
-    
+
+function powerUp() {
+  let pHitpoints = document.getElementById("playerHealth");
+  let x = Math.ceil(Math.random() * 5);
+  playerAttack *= (1.2);
+  playerAttack = Math.floor(playerAttack);
+  showDiv.innerHTML += `You increased your attack damage by 20% <br>`
+  healCooldown++;
+  log(healCooldown)
+  if (x != 5) {
+    pHitpoints.value -= bossAttack;
+    showDiv.innerHTML += `The boss hit you for ${bossAttack} damage <br>`
+  }
+  else showDiv.innerHTML += `You narrowly dodged the nurses vicious swipe <br>`
+  
 }
+
+// To do - 
 
---
+++ arraysloops/functiontestexample/lib.js
@@ -0,0 +1,8 @@
+function isSelected() {
+    let per = ["per", "pål", "geir", 'torstein']
+    console.log(per[3])
+    if (per[4]) return true
+    else if (undefined) return true
+    else return false
+    
+}
